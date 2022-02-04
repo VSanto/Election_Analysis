bb6bc13dd98d9e900a578e34f06f4b11fa0239d5
Valerie <92495807+VSanto@users.noreply.github.com>
2022-02-03 20:44:33 -0600

    Practice

---
+++ Looping_python.py
@@ -0,0 +1,18 @@
+counties=["Arapahoe","Denver","Jefferson"]
+# Set variable to 0
+x = 0
+# Test if x is less than or equal to 5
+while x <= 5:
+# If true print x
+    print(x)
+#Increment x by 1
+    x = x + 1
+# Will repeat this process until the condition false. When x is greater than 5, the loop stops
+numbers = [0,1,2,3,4]
+for num in numbers:
+    print(num)
+for num in range(5):
+    print(num)
+for i in range(len(counties)):
+    print(counties[i])
+counties_tuple = ("Arapahoe","Denver","Jefferson")   
---
+++ Python_practice.py
@@ -0,0 +1,105 @@
+print("Hello World")
+counties=["Arapahoe","Denver","Jefferson"]
+if counties[1]=='Denver':
+    print(counties[1])
+temperature = int(input("What is the temperature outside?"))
+if temperature >80:
+    print("Turn on the AC.")
+else:
+    print("Open the windows.")
+#What is the score?
+score = int(input("What is your test score?"))
+#Determine the grade.
+if score >=90:
+    print('Your gradde is an A.')
+else:
+    if score >=80:
+        print('Your grade is a B.')
+    else:
+        if score >=70:
+            print('Your grade is a C')
+        else: 
+            if score >=60:
+             print('Your grade is a D.')
+            else:
+                print('Your grade is an F.')
+# What is the score?
+score = int(input("What is your test score?"))
+# Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+elif score >= 80:
+    print('Your grade is a B.')
+elif score >= 70:
+    print('Your grade is a C.')
+elif score >= 60:
+    print('Your grade is a D')
+else:
+    print('Your grade is an F.')
+counties = ["Arapahoe", "Denver", "Jefferson"]
+if "Arapahoe" in counties: print("True")
+else: print("False")
+counties = ["Arapahoe","Denver","Jefferson "]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")
+if "Arapahoe" in counties and "El Paso" in counties:
+    print("Arapahoe and El Paso are in the list of counties.")
+else:
+    print("Arapahoe or El Paso is not in the list of counties.")
+if "Arapahoe" in counties or "El Paso" in counties:
+    print("Arapahoe or El Paso is in the list of counties.")
+else:
+    print("Arapahoe and El Paso are not in the list of counties.")
+if "Arapahoe" in counties and "El Paso" not in counties:
+   print("Only Arapahoe is in the list of counties.")
+else:
+    print("Arapahoe is in the list of counties and El Paso is not in the list of counties")
+for county in counties:
+    print(county)
+numbers = [0,1,2,3,4]
+for num in numbers:
+    print(num)
+for num in range(5):
+    print(num)
+# Iterate through lists    
+for i in range(len(counties)):
+    print(counties[i])   
+if len(counties) >2:
+    counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county in counties_dict:
+    print(county)
+# How many votes did you get?
+my_votes = int(input("How many votes did you get in the election? "))
+#  Total votes in the election
+total_votes = int(input("What is the total votes in the election? "))
+# Calculate the percentage of votes you received.
+percentage_votes = (my_votes / total_votes) * 100
+print("I received " + str(percentage_votes)+"% of the total votes.")
+for county in counties_dict.keys():     
+    print(county)
+for voters in counties_dict.values():
+    print(voters)
+for county in counties_dict:
+    print(counties_dict[county])
+for county in counties_dict:
+    print(counties_dict.get(county))
+for county, voters in counties_dict.items():
+    print(county,voters)
+for county, voters in counties_dict.items():
+    print(county,voters)
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829}
+             {"county":"Denver", "registered_voters": 463353}
+            {"county":"Jefferson", "registered_voters": 432438}]
+for county_dict in voting_data:
+    print(county_dict)
+for i in range(len(voting_data)):
+    print(voting_data[i]['county'])
+for county_dict in voting_data:
+    for value in county_dict.values():
+        print(value)
+for county_dict in voting_data:
+    print(county_dict['registered_voters'])
+for county_dict in voting_data:
+    print(county_dict['county'])
