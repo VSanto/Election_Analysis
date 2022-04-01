8eb0a496679f748350d75f46a043ccf68c8100c2
Valerie <92495807+VSanto@users.noreply.github.com>
2022-03-31 20:30:33 -0500

    Challenge Update in Progress

--- Resource/Python_practice.py
+++ Resource/Python_practice.py
@@ -76,7 +76,7 @@ my_votes = int(input("How many votes did you get in the election? "))
 total_votes = int(input("What is the total votes in the election? "))
 # Calculate the percentage of votes you received.
 percentage_votes = (my_votes / total_votes) * 100
-print("I received " + str(percentage_votes)+"% of the total votes.")
+print(f"I received {my_votes / total_votes *100}% of total votes")
 for county in counties_dict.keys():     
     print(county)
 for voters in counties_dict.values():
@@ -89,9 +89,9 @@ for county, voters in counties_dict.items():
     print(county,voters)
 for county, voters in counties_dict.items():
     print(county,voters)
-voting_data = [{"county":"Arapahoe", "registered_voters": 422829}
-             {"county":"Denver", "registered_voters": 463353}
-            {"county":"Jefferson", "registered_voters": 432438}]
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829}, 
+    {"county":"Denver", "registered_voters": 463353},
+    {"county":"Jefferson", "registered_voters": 432438}]
 for county_dict in voting_data:
     print(county_dict)
 for i in range(len(voting_data)):
@@ -103,3 +103,16 @@ for county_dict in voting_data:
     print(county_dict['registered_voters'])
 for county_dict in voting_data:
     print(county_dict['county'])
+counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters.")
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters.")
+# Candidate voting results
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+# Message votes to the thousands
+message_to_candidate = (
+    f"You received {candidate_votes:,} number of votes. "
+    f"The total number of votes in the election was {total_votes:,}. "
+    f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
